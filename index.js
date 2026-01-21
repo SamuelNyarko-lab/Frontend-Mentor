ed2892920b937d3d1f5fd910d14efc035aabb47f
Samuel Nyarko <samuelnyarko003@gmail.com>
2026-01-21 18:45:23 +0000

    Desktop complete

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 console.log("JS LINKED");
+
+let shareButton = document.querySelector(".share-button");
+let shareContent = document.querySelector(".share-content");
+
+shareButton.addEventListener("click", () => {
+  const isVisible = shareContent.style.visibility === "visible";
+  shareContent.style.visibility = isVisible ? "hidden" : "visible";
+
+  //shareButton.classList.toggle("focus"); //console.log(`Visibility: ${shareContent.style.visibility}`);
+  console.log(`Visibility: ${shareButton.classList}`);
+});
