54d02b8689860501994ccf1d5a20ce3d246ec682
Samuel Nyarko <samuelnyarko003@gmail.com>
2026-01-23 07:29:08 +0000

    Mobile complete

--- index.js
+++ index.js
@@ -2,11 +2,44 @@ console.log("JS LINKED");
 
 let shareButton = document.querySelector(".share-button");
 let shareContent = document.querySelector(".share-content");
+let mobileShareButton = document.querySelector(".mobile-share");
+let footer = document.getElementById("footer");
 
-shareButton.addEventListener("click", () => {
-  const isVisible = shareContent.style.visibility === "visible";
-  shareContent.style.visibility = isVisible ? "hidden" : "visible";
+function handleLayout() {
+  const screenWidth = window.innerWidth;
 
-  //shareButton.classList.toggle("focus"); //console.log(`Visibility: ${shareContent.style.visibility}`);
-  console.log(`Visibility: ${shareButton.classList}`);
-});
+  if (screenWidth > 400) {
+    mobileShareButton.style.visibility = "hidden";
+
+    shareButton.onclick = () => {
+      const isVisible = shareContent.style.visibility === "visible";
+      shareContent.style.visibility = isVisible ? "hidden" : "visible";
+    };
+  } else {
+    shareButton.onclick = () => {
+      footer.style.display = "none";
+      shareContent.style.display = "flex";
+
+      const isVisible = shareContent.style.visibility === "visible";
+
+      if (isVisible) {
+        shareContent.style.visibility = "hidden";
+        mobileShareButton.style.visibility = "hidden";
+      } else {
+        shareContent.style.visibility = "visible";
+        mobileShareButton.style.visibility = "visible";
+      }
+    };
+
+    mobileShareButton.onclick = () => {
+      shareContent.style.display = "none";
+      shareContent.style.visibility = "hidden";
+      mobileShareButton.style.visibility = "hidden";
+      footer.style.display = "flex";
+      footer.style.visibility = "visible";
+    };
+  }
+}
+
+handleLayout();
+window.addEventListener("resize", handleLayout);
